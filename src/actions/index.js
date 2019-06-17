f58976b5a50f7b13b6331d60a0851f8252059c2a
Awa Melvine <melvineawa@gmail.com>
2019-06-17 13:28:07 +0100

    Define the reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
+export const INCREMENT_IF_ODD = "INCREMENT_IF_ODD";
+export const INCREMENT_ASYNC = "INCREMENT_ASYNC";
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
 
 // Our action creators will return
 // an action packet that our reducer will
@@ -9,10 +11,18 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = () => {
-  // Fill in this function
-};
+export const increment = () => ({
+  type: INCREMENT
+});
 
-export const decrement = () => {
-  // Fill in this function
-};
+export const decrement = () => ({
+  type: DECREMENT
+});
+
+export const incrementIfOdd = () => ({
+  type: INCREMENT_IF_ODD
+});
+
+export const incrementAsync = () => ({
+  type: INCREMENT_ASYNC
+});
